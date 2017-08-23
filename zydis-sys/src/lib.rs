ca59066272479878a6b6ca4a7b3befd5d8cc3cd6
th0rex <c0untlizzi@gmail.com>
2017-08-23 16:51:53 +0200

    Add TryFrom implementation for ZydisEncoderRequest

--- zydis-sys/src/lib.rs
+++ zydis-sys/src/lib.rs
@@ -1,5 +1,29 @@
+#![feature(try_from)]
+
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+use std::convert::TryFrom;
+
+macro_rules! check {
+    ($expression:expr, $ok:expr) => {
+        match $expression as _ {
+            ZYDIS_STATUS_SUCCESS => Ok($ok),
+            e => Err(e),
+        }
+    };
+}
+
+impl TryFrom<ZydisDecodedInstruction> for ZydisEncoderRequest {
+    type Error = ZydisStatusCode;
+
+    fn try_from(value: ZydisDecodedInstruction) -> Result<Self, ZydisStatusCode> {
+        unsafe {
+            let mut ret = std::mem::uninitialized();
+            check!(ZydisEncoderDecodedInstructionToRequest(&value, &mut ret), ret)
+        }
+    }
+}
